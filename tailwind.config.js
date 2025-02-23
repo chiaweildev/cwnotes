d5bc0d8854e6cda48343f5cc33638ec97e040654
chiaweildev <chiaweildev@gmail.com>
2025-02-23 22:51:19 +0800

    chore: re-add Tailwind CSS configuration and global styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    screens: {
+      'mobile': '420px', // adjust pixel value as needed
+    },
+  },
+  plugins: [],
+}
+
