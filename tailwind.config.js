651692d71e75a35523bc441de23434925b91eeea
chiaweildev <chiaweildev@gmail.com>
2025-02-24 01:57:07 +0800

    chore: update Tailwind CSS configuration and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
+  ],
   theme: {
     extend: {},
     screens: {
